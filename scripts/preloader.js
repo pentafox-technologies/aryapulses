9df584e074bcc721799f116465095189556b42bf
Shidiq Ahmed <shidiq@pentafox.in>
2023-05-16 09:53:30 +0530

    Updates

--- scripts/preloader.js
+++ scripts/preloader.js
@@ -1,10 +1,19 @@
+// var body = document.querySelector("body");
+// document.onreadystatechange = function () {
+//   if (document.readyState === "complete") {
+//     body.classList.add("preloader_ready");
+//     setTimeout(function () {
+//       body.classList.remove("preloader_active");
+//       body.classList.remove("preloader_ready");
+//     }, 1000);
+//   }
+// };
+
 var body = document.querySelector("body");
-document.onreadystatechange = function () {
-  if (document.readyState === "complete") {
-    body.classList.add("preloader_ready");
-    setTimeout(function () {
-      body.classList.remove("preloader_active");
-      body.classList.remove("preloader_ready");
-    }, 1000);
-  }
+window.onload = function () {
+  body.classList.add("preloader_ready");
+  setTimeout(function () {
+    body.classList.remove("preloader_active");
+    body.classList.remove("preloader_ready");
+  }, 1000);
 };
